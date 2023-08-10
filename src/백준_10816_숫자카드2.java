bc34844edf30e88aaff0490f6f04f9bf9cf55148
HH <hhdev1117@gmail.com>
2023-08-10 22:11:09 +0900

    no message

--- src/백준_10816_숫자카드2.java
+++ src/백준_10816_숫자카드2.java
@@ -1,59 +1,60 @@
-import java.io.*;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Scanner;
 
 public class 백준_10816_숫자카드2 {
 
-  public static void main(String[] args) throws Exception {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    String[] nArray = br.readLine().split(" ");
+  public static void main(String[] args) {
 
-    int M = Integer.parseInt(br.readLine());
-    String[] mArray = br.readLine().split(" ");
+    Scanner sc = new Scanner(System.in);
 
-    Arrays.sort(nArray, new Comparator<String>() {
-      @Override
-      public int compare(String o1, String o2) {
-        return Long.compare(Long.parseLong(o1), Long.parseLong(o2));
-      }
-    });
+    StringBuilder sb = new StringBuilder();
 
-    for(String str : nArray) System.out.print (str + " ");
+    int N = sc.nextInt();
+    int[] nArray = new int[N];
+    for(int i = 0; i < N; i++) nArray[i] = sc.nextInt();
+
+    int M = sc.nextInt();
+
+    Arrays.sort(nArray);
 
     for(int i = 0; i < M; i++) {
-      getCardCount(mArray[i], nArray);
+      int number = sc.nextInt();
+
+      sb.append(getUpperBound(number, nArray) - getLowerBound(number, nArray)).append(" ");
     }
+
+    System.out.println(sb);
   }
 
-  public static int getCardCount(String num, String[] nArray) {
-    int max = nArray.length;
-    int min = 0;
-    int mid = 0;
+  public static int getLowerBound(int number, int[] nArray) {
+    int right = nArray.length;
+    int left = 0;
 
-    int number = Integer.parseInt(num);
-
-    int answer = 0;
-
-    while(true) {
-      mid = (int) Math.floor((float) (max + min) / (float) 2);
-      int n = Integer.parseInt(nArray[mid]);
-      if(n > number) {
-        //System.out.println("1 mid > number ===> mid : " + mid + " / max : " + max + " / min : " + min);
-        max = mid;
-      } else if(n < number) {
-        //System.out.println("2 mid < number ===> mid : " + mid + " / max : " + max + " / min : " + min);
-        min = mid;
+    while(left < right) {
+      int mid = (right + left)/2;
+      if(nArray[mid] >= number) {
+        right = mid;
       } else {
-        //System.out.println("3 mid = number ===> mid : " + mid + " / max : " + max + " / min : " + min);
-        answer++;
-        break;
+        left = mid + 1;
       }
-
-      if(mid == (long) Math.floor((float) (max + min) / (float) 2)) return 0;
     }
 
-    System.out.println(mid);
-    return answer;
+    return left;
+  }
+
+  public static int getUpperBound(int number, int[] nArray) {
+    int right = nArray.length;
+    int left = 0;
+
+    while(left < right) {
+      int mid = (right + left)/2;
+      if(nArray[mid] <= number) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    return left;
   }
 }
---
+++ src/백준_1208_부분수열의합2.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 백준_1208_부분수열의합2 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+  }
+}
